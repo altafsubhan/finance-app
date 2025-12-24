eea8fdb214a1762b087673554206b4e6f82a9a9d
Subhan Altaf <altas7264@gmail.com>
2025-12-23 23:20:26 -0800

    Fix: Add missing isSelectionMode props to filtered view TransactionTable call

--- components/TransactionList.tsx
+++ components/TransactionList.tsx
@@ -481,6 +481,8 @@ export default function TransactionList({ transactions, categories, onEdit, onDe
           sortField={sortField}
           sortDirection={sortDirection}
           onSort={handleSort}
+          isSelectionMode={isSelectionMode}
+          setIsSelectionMode={setIsSelectionMode}
         />
       </div>
 
